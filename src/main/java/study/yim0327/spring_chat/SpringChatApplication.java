861c3cc8de906dbd2813a4aa8e83a807b09825a8
yim0327 <yim0327@naver.com>
2025-11-10 11:07:25 +0900

    feat: initialize Spring Boot project

---
+++ src/main/java/study/yim0327/spring_chat/SpringChatApplication.java
@@ -0,0 +1,13 @@
+package study.yim0327.spring_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/yim0327/spring_chat/SpringChatApplicationTests.java
@@ -0,0 +1,13 @@
+package study.yim0327.spring_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
